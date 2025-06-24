021d883c3c4e87adb81b21b12a784d1a4aa1045a
Riesa Cassano-Coleman <rcassan2@ur.rochester.edu>
2025-06-24 12:18:00 -0400

    Fix names

--- primary_data/E124_raw_data_combining.py
+++ primary_data/E124_raw_data_combining.py
@@ -5,14 +5,14 @@ import numpy as np
 import pandas as pd
 
 # load the file list
-folder = '../data/Sarah_E124/raw_data/'
+folder = 'raw_data_anonymized/'
 file_list = os.listdir(folder)
 
 df_list = []
 
-for entry in file_list:
-#for i in range(2):
-#	entry = file_list[i]
+#for entry in file_list:
+for i in range(2):
+	entry = file_list[i]
 	filepath = folder + entry
 	print(entry)
 	if '.xlsx' in filepath: this_file = pd.read_excel(filepath)
@@ -33,19 +33,19 @@ for entry in file_list:
 	# condition is under 'scramble_p_3', 'scramble_m_2'... seems like all options are there as empty columns
 	
 	#this_file1 = this_file[['exp_subject_id', 'Task_Name', 'Trial_Nr', 'response', 'response_seg2']]
-	if 'response_seg2' in this_file.columns:
-		this_file1 = this_file[['exp_subject_id', 'Task_Name', 'Trial_Nr', 'response', 'response_seg2']]
-	else: 
-		this_file1 = this_file[['exp_subject_id', 'Task_Name', 'Trial_Nr', 'response']]
-		this_file1['response_seg2'] = np.nan
-	this_file2 = this_file.filter(like = 'scramble')
+	#if 'response_seg2' in this_file.columns:
+	#	this_file1 = this_file[['exp_subject_id', 'Task_Name', 'Trial_Nr', 'response', 'response_seg2']]
+	#else: 
+	#	this_file1 = this_file[['exp_subject_id', 'Task_Name', 'Trial_Nr', 'response']]
+	#	this_file1['response_seg2'] = np.nan
+	#this_file2 = this_file.filter(like = 'scramble')
 
-	this_file_selected = pd.concat([this_file1, this_file2], axis=1)
-	df_list.append(this_file_selected)
+	#this_file_selected = pd.concat([this_file1, this_file2], axis=1)
+	#df_list.append(this_file_selected)
 	#print(this_file_selected)
 	
 	
 
 # concatenate
-big_df = pd.concat(df_list, ignore_index = True)
-big_df.to_csv('../data/Sarah_E124/combined_raw.csv', index = False)
+#big_df = pd.concat(df_list, ignore_index = True)
+#big_df.to_csv('raw_combined.csv', index = False)
