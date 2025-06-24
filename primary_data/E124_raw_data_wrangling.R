4ff01c408855c7045b8cfea5653a37eb99947840
Riesa Cassano-Coleman <rcassan2@ur.rochester.edu>
2025-06-24 12:58:32 -0400

    Save trial-level data files

--- primary_data/E124_raw_data_wrangling.R
+++ primary_data/E124_raw_data_wrangling.R
@@ -88,8 +88,8 @@ print(length(unique(filter(prediction, Musician == 'Yes')$exp_subject_id))) # 53
 print(length(unique(filter(categorization, Musician == 'Yes')$exp_subject_id))) # 54 / 106 are musicians
 
 # save the wrangled data
-write_csv(memory, '../data/Sarah_E124/memory.csv')
-write_csv(prediction, '../data/Sarah_E124/prediction.csv')
-write_csv(categorization, '../data/Sarah_E124/categorization.csv')
+write_csv(memory, '../E1-E2-E4/memory.csv')
+write_csv(prediction, '../E1-E2-E4/prediction.csv')
+write_csv(categorization, '../E1-E2-E4/categorization.csv')
 
 
